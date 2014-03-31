ffa835b373b6473b83658005360894d1c80b18e2
longtai.tw@gmail.com <longtai.tw@gmail.com@18034c6f-9995-0d2e-7eb0-a8f5cd9e8ab8>
2014-03-31 15:32:33 +0000

    [change] 新增試藥編輯欄位檢查

--- app/Model/Reagent.php
+++ app/Model/Reagent.php
@@ -2,6 +2,21 @@
 class Reagent extends AppModel {
     public $name = 'Reagent';
     public $belongsTo = 'Chemical';
+    public $validate = array(
+    		'id' => array(
+    				'required' => array(
+    						'rule' => array('notEmpty'),
+    						'message' => '試藥代號請勿空白'
+    				)
+    		),
+    		'name' => array(
+    				'required' => array(
+    						'rule' => array('notEmpty'),
+    						'message' => '試藥名稱請勿空白'
+    				)
+    		),    		
+    
+    );    
 	public function beforeSave($options = array()) {
 		if (!isset($this->data[$this->alias]['id'])) {
 			$this->data[$this->alias]['create_time'] = date();
